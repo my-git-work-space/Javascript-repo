4745ebc7b45a8879ff7879e9a7e3e91d8b14a560
my-git-work-space <111415891+my-git-work-space@users.noreply.github.com>
2022-10-12 14:16:05 +0530

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,21 @@
+let result=document.getElementById("inputtext");
+let display=(number)=>{
+    result.value+=number;  
+}
+let Result=()=>{
+try{
+    result.value=eval(result.value);
+}
+catch(err){
+    alert("enter the valid input!");
+}
+
+}
+function clr(){
+    result.value="";
+}
+
+function del(){
+
+    result.value=result.value.slice(0,-1);
+}
